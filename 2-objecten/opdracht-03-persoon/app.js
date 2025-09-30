0c6236e2eb1af1eb5c4a7dd236a20911f2a75249
EnesAydogan14 <9027461@student.zadkine.nl>
2025-09-30 11:45:20 +0200

    opdr 1-4

--- 2-objecten/opdracht-03-persoon/app.js
+++ 2-objecten/opdracht-03-persoon/app.js
@@ -1,25 +1,38 @@
-// Stap 1
+var person = {
+    name: "Jan Jansen",
+    age: 30,
+    birthDate: "1994-01-15",
+    gender: "Male",
+    alive: true,
+    hobbies: ["Football", "Reading", "Cooking", "Cycling"],
 
-// Maak een persoon object waar je allemaal eigenschappen van een persoon in kunt opslaan en tonen op het scherm.
+    changeName: function(newName) {
+        this.name = newName;
+        showPerson();
+    },
 
-// Het object moet de volgende properties bevatten:
+    changeAliveStatus: function(status) {
+        this.alive = status;
+        showPerson();
+    },
 
-//     Naam: de naam van de persoon (string)
-//     Leeftijd: de leeftijd van de persoon (number)
-//     Geboortedatum: de geboortedatum van de persoon (string)
-//     Geslacht: het geslacht van de persoon (string)
-//     In leven: geef aan of de persoon nog leeft (boolean)
-//     Hobby’s: 4 hobby’s van de persoon (array)
+    addHobby: function(hobby) {
+        this.hobbies.push(hobby);
+        showPerson();
+    }
+};
 
-// Toon elke property op het scherm door het in een p-tag te plaatsen. Hiervoor gebruik je de div-tag van de vorige opdracht.
-// Stap 2:
+function showPerson() {
+    var outputDiv = document.querySelector('.output');
+    outputDiv.innerHTML = `
+        <p>Name: ${person.name}</p>
+        <p>Age: ${person.age}</p>
+        <p>Birth date: ${person.birthDate}</p>
+        <p>Gender: ${person.gender}</p>
+        <p>Alive: ${person.alive ? "Yes" : "No"}</p>
+        <p>Hobbies: ${person.hobbies.join(', ')}</p>
+    `;
+}
 
-// Het object moet ook de volgende methods bevatten:
+document.addEventListener('DOMContentLoaded', showPerson);
 
-//     Een method (functie) die de naam moet veranderen. De naam moet je via de parameters kunnen meegeven.
-//     Een method (functie) die het ‘in leven’ property kan veranderen.
-//     Een method (functie) die een extra hobby toevoegt aan de array. Een hobby moet je via de parameters kunnen meegeven.
-
-// Toon de wijzigingen op het scherm.
-
-// Gebruik geen arrow function in een object!
