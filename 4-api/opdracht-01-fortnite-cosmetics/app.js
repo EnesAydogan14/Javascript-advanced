53abc8ef5b31b96d5267a7bf2d00253d19145bf4
EnesAydogan14 <9027461@student.zadkine.nl>
2025-10-01 11:27:15 +0200

    opdrachten

---
+++ 4-api/opdracht-01-fortnite-cosmetics/app.js
@@ -0,0 +1,20 @@
+let output = document.querySelector('#cosmetics-container');
+ 
+fetch('https://fortnite-api.com/v2/cosmetics/new')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+ 
+    for (let item of data.data.items.br) {
+      output.innerHTML += `
+        <div class="card">
+          <img src="${item.images.icon}" alt="${item.name}">
+          <h2>${item.name}</h2>
+          <p>Rarity: ${item.rarity.value}</p>
+          <p>Type: ${item.type.value}</p>
+        </div>
+      `;
+    }
+  });
+ 
+ 
---
+++ 4-api/opdracht-02-countries/app.js
@@ -0,0 +1,17 @@
+let output = document.querySelector('#countries-container');
+ 
+fetch('https://restcountries.com/v3.1/all?fields=name,flags')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+ 
+    for (let item of data) {
+      output.innerHTML += `
+        <div class="card">
+      <img src="${item.flags.png}" alt="${item.name}">
+          <h2>${item.name.common}</h2>
+        </div>
+      `;
+    }
+  });
+ 
